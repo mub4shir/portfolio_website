c98c83cefbdceff8e6eb36462bcf7014bf6715d6
mubashir <mubashir11131719@gmail.com>
2021-09-08 20:53:53 +0530

    tech section done

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import {
+  DiCode,
+  DiFirebase,
+  DiJava,
+  DiJavascript,
+  DiNodejs,
+  DiReact,
+  DiZend,
+} from 'react-icons/di';
 import {
   Section,
   SectionDivider,
@@ -17,11 +25,44 @@ import {
 const Technologies = () => (
   <Section id="tech">
     <SectionDivider />
+    <br />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
       I've worked with a range of technologies in web development world. From
       Back-end To Design
     </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node.js and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiJava size="3rem" />
+        <ListContainer>
+          <ListTitle>Programming</ListTitle>
+          <ListParagraph>
+            Experience wirh <br />
+            Java,JavaScript and C++
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
   </Section>
 );
 
