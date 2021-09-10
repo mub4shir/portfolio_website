b0e277862f917138f99f57b76248d613e5389670
mubashir <mubashir11131719@gmail.com>
2021-09-10 17:31:33 +0530

    added favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,12 @@
 import Theme from '../styles/theme';
-
+const prefix = '/portfolio_website';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <link rel="shortcut icon" href={prefix + '/favicon.ico'} />
       <Theme>
         <Component {...pageProps} />
       </Theme>
     </>
   );
 }
- 
